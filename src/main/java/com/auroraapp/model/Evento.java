a0d6e6f7fb4d2aa02b51b75c15c48d2f7e735749
PedroASouzaDev <pedro.amendonca@souunit.com.br>
2025-10-13 17:44:15 -0300

    add: classe Evento

--- src/main/java/com/auroraapp/model/Evento.java
+++ src/main/java/com/auroraapp/model/Evento.java
@@ -1,5 +1,23 @@
 package com.auroraapp.model;
 
+import com.auroraapp.enums.Categoria;
+
+import java.util.LinkedList;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class Evento {
-    
+    private String nome;
+    private LinkedList<Usuario> participantes;
+    private LinkedList<Organizador> organizadores;
+    private Date dataInicio;
+    private Date dataFim;
+    private LinkedList<Categoria> categorias;
+    private Double valorIngresso;  
 }
