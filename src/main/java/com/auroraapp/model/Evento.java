31f43f6c1121577153f4432da6cb1a38aa33b54c
PedroASouzaDev <pedro.amendonca@souunit.com.br>
2025-10-20 15:50:47 -0300

    add: dependências JPA no pom.xml e atualização da classe evento

--- src/main/java/com/auroraapp/model/Evento.java
+++ src/main/java/com/auroraapp/model/Evento.java
@@ -1,24 +1,42 @@
 package com.auroraapp.model;
 
 import java.util.LinkedList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
 import java.time.LocalDate;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "evento")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Evento {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private LocalDate dataInicio;
     private LocalDate dataFim;
     private Double valorIngresso;
+
+    @ManyToMany(mappedBy = "evento_participante")
     private LinkedList<Usuario> participantes;
+
+    @ManyToMany(mappedBy = "evento_organizador")
     private LinkedList<Organizador> organizadores;
+
+    @ManyToMany(mappedBy = "evento_categoria")
     private LinkedList<Categoria> categorias;
 }
