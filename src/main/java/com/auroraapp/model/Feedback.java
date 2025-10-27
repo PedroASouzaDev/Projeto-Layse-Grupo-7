8f9ae864b2c34abaae385380c6a86bb50c50ec03
PedroASouzaDev <pedro.amendonca@souunit.com.br>
2025-10-27 16:04:07 -0300

    add: classe Feedback no modelo JPA

--- src/main/java/com/auroraapp/model/Feedback.java
+++ src/main/java/com/auroraapp/model/Feedback.java
@@ -1,5 +1,32 @@
 package com.auroraapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "feedbacks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Feedback {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Evento evento;
+
+    @ManyToOne
+    private Participante participante;
 }
